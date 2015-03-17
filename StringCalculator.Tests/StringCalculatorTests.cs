cc6f47b9f4cc994a38c37ca95c52de6485e32241
azl <AdamL@sfwltd.co.uk>
2015-03-17 17:49:59 +0000

    Adding first test for empty string

---
+++ StringCalculator.Tests/StringCalculatorTests.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Xunit;
+using StringCalculator;
+using FluentAssertions;
+
+namespace StringCalculator.Tests
+{
+    public class StringCalculatorTests
+    {
+        [Fact]
+        public void Returns0WhenEmptyString()
+        {
+            StringCalculator calc = new StringCalculator();
+            calc.add("").Should().Be(0);      
+        }
+    }
+}
---
+++ StringCalculator/StringCalculator.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace StringCalculator
+{
+    public class StringCalculator
+    {
+        // Adds any digits it finds in the input string, providing the answer as an integer
+        public int add(string input)
+        {
+            return 0;
+        }
+    }
+}
